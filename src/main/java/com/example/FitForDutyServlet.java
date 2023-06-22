78de13bea14630cf82948ccd32343600f3ec41b6
ristabel <112583477+ristabel@users.noreply.github.com>
2023-06-22 03:00:07 -0600

    Update FitForDutyServlet.java

--- src/main/java/com/example/FitForDutyServlet.java
+++ src/main/java/com/example/FitForDutyServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Scanner;
 
 public class FitForDutyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
