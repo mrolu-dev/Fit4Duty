925e08c51365c8f47014887f92a1210680b7287e
ristabel <112583477+ristabel@users.noreply.github.com>
2023-06-22 01:00:23 -0600

    Update FitForDutyServlet.java

--- src/main/java/com/example/FitForDutyServlet.java
+++ src/main/java/com/example/FitForDutyServlet.java
@@ -15,6 +15,23 @@ public class FitForDutyServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        // Get the path to the index.html file
+        String indexPath = getServletContext().getRealPath("/fit-for-duty.html");
+    
+        // Read the contents of the index.html file
+        try (BufferedReader reader = new BufferedReader(new FileReader(indexPath))) {
+            String line;
+            StringBuilder content = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        
+        // Send the index.html content as the response
+        PrintWriter writer = response.getWriter();
+        writer.println(content.toString());
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 
         out.println("<html>");
         out.println("<head>");
