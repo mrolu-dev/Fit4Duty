547225b6d84c380d093a28834deefffdae8e01ef
ristabel <112583477+ristabel@users.noreply.github.com>
2023-06-22 01:14:20 -0600

    Update FitForDutyServlet.java

--- src/main/java/com/example/FitForDutyServlet.java
+++ src/main/java/com/example/FitForDutyServlet.java
@@ -8,31 +8,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class FitForDutyServlet extends HttpServlet {
+public class Fit4DutyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // Set the content type of the response
-    response.setContentType("text/html");
-
-    // Get the path to the fit-for-duty.html file
-    String filePath = getServletContext().getRealPath("/fit-for-duty.html");
-
-    // Read the contents of the HTML file
-    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-        String line;
-        StringBuilder content = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            content.append(line);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+                 // Get the path to the fit-for-duty.html file
+        String filePath = getServletContext().getRealPath("/fit-for-duty.html");
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            StringBuilder content = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
         }
-
-        // Send the HTML content as the response
-        PrintWriter writer = response.getWriter();
+        PrintWriter out = response.getWriter();
         writer.println(content.toString());
-    } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
         e.printStackTrace();
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-         out.println("<html>");
+    }
+
+        out.println("<html>");
         out.println("<head>");
         out.println("<title>Fit4Duty</title>");
         out.println("</head>");
@@ -42,5 +40,4 @@ public class FitForDutyServlet extends HttpServlet {
         out.println("</body>");
         out.println("</html>");
     }
-}
 }
