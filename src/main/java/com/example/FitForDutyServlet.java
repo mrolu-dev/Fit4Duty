dc3796394a8ab2df6df68c052540278e948a7743
ristabel <112583477+ristabel@users.noreply.github.com>
2023-06-22 02:56:05 -0600

    Update FitForDutyServlet.java

--- src/main/java/com/example/FitForDutyServlet.java
+++ src/main/java/com/example/FitForDutyServlet.java
@@ -14,33 +14,33 @@ public class FitForDutyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // Retrieve the selected options from the request parameters
-        String physicalSymptoms = request.getParameter("physicalSymptoms");
-        String cognitiveFunction = request.getParameter("cognitiveFunction");
-        String emotionalState = request.getParameter("emotionalState");
-        String workloadResponsibilities = request.getParameter("workloadResponsibilities");
-        String environmentalFactors = request.getParameter("environmentalFactors");
-        String personalFactors = request.getParameter("personalFactors");
-        
-        // Perform the fitness calculation using FitForDutyApp
-        FitForDutyApp fitForDutyApp = new FitForDutyApp();
-        String result = fitForDutyApp.selectOption(
-                physicalSymptoms,
-                cognitiveFunction,
-                emotionalState,
-                workloadResponsibilities,
-                environmentalFactors,
-                personalFactors
-        );
-        
-        // Set the result as a request attribute
-        request.setAttribute("fitForDutyResult", result);
-        
-        // Forward the request to the JSP for rendering
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/result.jsp");
-        dispatcher.forward(request, response);
-    }
+        throws ServletException, IOException {
+    // Retrieve the selected options from the request parameters
+    String physicalSymptoms = request.getParameter("physicalSymptoms");
+    String cognitiveFunction = request.getParameter("cognitiveFunction");
+    String emotionalState = request.getParameter("emotionalState");
+    String workloadResponsibilities = request.getParameter("workloadResponsibilities");
+    String environmentalFactors = request.getParameter("environmentalFactors");
+    String personalFactors = request.getParameter("personalFactors");
+
+    // Perform the fitness calculation using FitForDutyApp
+    FitForDutyApp fitForDutyApp = new FitForDutyApp();
+    String result = fitForDutyApp.selectOption(
+        physicalSymptoms,
+        cognitiveFunction,
+        emotionalState,
+        workloadResponsibilities,
+        environmentalFactors,
+        personalFactors
+    );
+
+    // Set the result as a request attribute
+    request.setAttribute("fitForDutyResult", result);
+
+    // Forward the request to the JSP for rendering
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/result.jsp");
+    dispatcher.forward(request, response);
+}
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
