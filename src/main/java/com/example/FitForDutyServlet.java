321cda8370513e8a72fc9240b7dac8818a036667
ristabel <112583477+ristabel@users.noreply.github.com>
2023-06-22 01:03:18 -0600

    Update FitForDutyServlet.java

--- src/main/java/com/example/FitForDutyServlet.java
+++ src/main/java/com/example/FitForDutyServlet.java
@@ -11,37 +11,26 @@ import javax.servlet.http.HttpServletResponse;
 public class FitForDutyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        // Get the path to the index.html file
-        String indexPath = getServletContext().getRealPath("/fit-for-duty.html");
-    
-        // Read the contents of the index.html file
-        try (BufferedReader reader = new BufferedReader(new FileReader(indexPath))) {
-            String line;
-            StringBuilder content = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                content.append(line);
-            }
-        
-        // Send the index.html content as the response
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // Set the content type of the response
+    response.setContentType("text/html");
+
+    // Get the path to the fit-for-duty.html file
+    String filePath = getServletContext().getRealPath("/fit-for-duty.html");
+
+    // Read the contents of the HTML file
+    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        String line;
+        StringBuilder content = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            content.append(line);
+        }
+
+        // Send the HTML content as the response
         PrintWriter writer = response.getWriter();
         writer.println(content.toString());
     } catch (FileNotFoundException e) {
         e.printStackTrace();
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Fit4Duty</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Welcome to Fit4Duty</h1>");
-        out.println("<p>This is the Fit4Duty servlet.</p>");
-        out.println("</body>");
-        out.println("</html>");
     }
 }
-
