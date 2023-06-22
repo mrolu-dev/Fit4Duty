5e210b0e03580deaf94fc753283171c5cfd366df
ristabel <112583477+ristabel@users.noreply.github.com>
2023-06-22 01:23:21 -0600

    Update FitForDutyServlet.java

--- src/main/java/com/example/FitForDutyServlet.java
+++ src/main/java/com/example/FitForDutyServlet.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileReader;
-import java.io.*
+import java.io.*;
 
 
 import javax.servlet.ServletException;
